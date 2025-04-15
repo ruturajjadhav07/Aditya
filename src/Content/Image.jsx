8cd45d0f9d84368adbedcdfc0a2adaab1c287d9f
“[github]” <ruturajjadhav122@gmail.com>
2025-04-15 12:02:17 +0530

    added anime effect

--- src/Content/Image.jsx
+++ src/Content/Image.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "aos/dist/aos.css";
+import AOS from "aos";
 import Image1 from "../assets/image.jpg";
 
 const Image = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <div className="container py-5 mt-2 mb-2" id="about">
       <div className="d-flex flex-wrap justify-content-center align-items-start gap-4">
@@ -13,12 +21,14 @@ const Image = () => {
             src={Image1}
             alt="Main"
             className="img-fluid rounded"
+            data-aos="fade-right"
             style={{ width: "100%", maxWidth: "600px" }}
           />
           <img
             src={Image1}
             alt="Portrait 1"
             className="img-fluid rounded align-self-end"
+            data-aos="fade-right"
             style={{ width: "100%", maxWidth: "400px" }}
           />
         </div>
@@ -27,14 +37,16 @@ const Image = () => {
         <div className="d-flex flex-column gap-4">
           <img
             src={Image1}
-            alt="Portrait 1"
+            alt="Portrait 2"
             className="img-fluid rounded"
+            data-aos="fade-left"
             style={{ width: "100%", maxWidth: "400px" }}
           />
           <img
             src={Image1}
-            alt="Portrait 2"
+            alt="Portrait 3"
             className="img-fluid rounded"
+            data-aos="fade-left"
             style={{ width: "100%", maxWidth: "600px" }}
           />
         </div>
